25acdc65401e480cfe93e62cfa4c655742e92279
Matthew Early <matthewjearly@gmail.com>
2022-01-24 14:11:12 -0500

    Add project files.

---
+++ demo_rpg/hitpointtypes.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <cstdint>
+
+typedef std::uint16_t hptype;
---
+++ demo_rpg/hp.h
@@ -0,0 +1,51 @@
+#pragma once
+#include "hitpointtypes.h"
+
+class hp {
+public:
+	// returns true if set successfully
+	bool setMaxHP(hptype new_max_hp) {
+		if (new_max_hp < 1)
+			return false;
+
+		MaxHP = new_max_hp;
+
+		if (CurrentHP > MaxHP)
+			CurrentHP = MaxHP;
+
+		return true;
+	}
+
+	hptype getMaxHp() {
+		return MaxHP;
+	}
+
+	hptype getCurrentHP() {
+		return CurrentHP;
+	}
+
+	void takeDamage(hptype damage) {
+		if (damage > CurrentHP) {
+			CurrentHP = 0;
+			return;
+		}
+
+		CurrentHP -= damage;
+	}
+	
+	void heal(hptype amount) {
+		if (amount + CurrentHP > MaxHP) {
+			CurrentHP = MaxHP;
+			return;
+		}
+
+		CurrentHP += amount;
+	}
+
+private:
+	hptype CurrentHP;
+	hptype MaxHP;
+};
+
+
+
