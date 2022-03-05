07044affb5e421d786dc645014b3845c535af8b3
Matthew Early <matthewjearly@gmail.com>
2022-03-05 06:44:08 -0500

    ability list now pointers

--- demo_rpg/cleric.cpp
+++ demo_rpg/cleric.cpp
@@ -4,12 +4,12 @@
 Cleric::Cleric() : PlayerCharacterDelegate() {
   MP = std::make_unique<PointWell>(BASEMP, BASEMP);  // be sure to init before PCCONSTRUCT MACRO
   PCCONSTRUCT;
-  Abilities.emplace_back("Heal", 2u, nullptr, 2u, 1u, ABILITYTARGET::ALLY, ABILITYSCALER::INT);
+  Abilities.emplace_back(new Ability("Heal", 2u, nullptr, 2u, 1u, ABILITYTARGET::ALLY, ABILITYSCALER::INT));
 }
 
 void Cleric::level_up() noexcept {
   LEVELUP;
   if (GetLevel() == 2) {
-    Abilities.emplace_back("Smite", 2u, nullptr, 2u, 1u, ABILITYTARGET::ENEMY, ABILITYSCALER::INT);
+    Abilities.emplace_back(new Ability("Smite", 2u, nullptr, 2u, 1u, ABILITYTARGET::ENEMY, ABILITYSCALER::INT));
   }
 }
--- demo_rpg/wizard.cpp
+++ demo_rpg/wizard.cpp
@@ -4,12 +4,12 @@
 Wizard::Wizard() : PlayerCharacterDelegate() {
   MP = std::make_unique<PointWell>(BASEMP, BASEMP);  // be sure to init before PCCONSTRUCT MACRO
   PCCONSTRUCT;
-  Abilities.emplace_back("Firebolt", 4u, nullptr, 2u, 1u, ABILITYTARGET::ENEMY, ABILITYSCALER::INT);
+  Abilities.emplace_back(new Ability("Firebolt", 4u, nullptr, 2u, 1u, ABILITYTARGET::ENEMY, ABILITYSCALER::INT));
 }
 void Wizard::level_up() noexcept {
   LEVELUP;
   if (GetLevel() == 2) {
-    Abilities.emplace_back("IceBolt", 6u, nullptr, 3u, 1u, ABILITYTARGET::ENEMY, ABILITYSCALER::INT);
+    Abilities.emplace_back(new Ability("IceBolt", 6u, nullptr, 3u, 1u, ABILITYTARGET::ENEMY, ABILITYSCALER::INT));
     increase_stats(0, 1);
   }
 }
